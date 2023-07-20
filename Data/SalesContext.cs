5289ca88bdc59adf93da28f857615dfe9831fb93
Carlos <aguirrecarlos1995@gmail.com>
2023-07-20 01:09:29 -0300

    asd

---
+++ Data/SalesContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiEntityFrameworkDockerSqlServer.Data
+{
+    public class SalesContext : DbContext
+    {
+        public SalesContext(DbContextOptions<SalesContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,4)");
+        }
+        public DbSet<Product>? Products { get; set; }
+    }
+}
